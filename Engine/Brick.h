f3ee98c4a34ecbca45b544d8559aadfcb72944f2
Akashdeepsingh <Akashdeepsignh841@outlook.com>
2017-11-03 21:03:49 +0530

    Created Brick class.

---
+++ Engine/Brick.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "RectF.h"		//Every brick is a rectangle.
+#include "Colors.h"		//Every brick is colored so that we can see.
+#include "Graphics.h"	//For getting the gfx object to draw on.
+class Brick
+{
+public:
+	Brick(const RectF& rect_in, Color color_in);		//The constructor. No need to make color reference because it is just 3 or 4 numbers.
+	void Draw(Graphics& gfx)const;						//Draw the brick on the frame. So refernce to the one and only frame is passed. Since the brick is unchanged, function ends with const.
+private:
+	RectF rect;
+	Color color;
+	bool destroyed = false;		//For knowing if a brick is destroyed or not. Default is false because when created it must be visible.
+};
